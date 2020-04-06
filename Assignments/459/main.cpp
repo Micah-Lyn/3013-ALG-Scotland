a7b1242ba19ff9ebd60403c8a4c32e3fecef47e0
Micah-Lyn <micahlyns@gmail.com>
2020-04-05 22:31:10 -0500

    .

--- Assignments/459/main.cpp
+++ Assignments/459/main.cpp
@@ -38,39 +38,45 @@ vector<vii> AdjList; // AdjList stores our edge lists
  *  int u - starting node id
  * 
  */
-void dfs(int u) {
+void dfs(int u)
+{
     // In DFS we use a stack structure to keep track of who to visit next.
     // Recursive acts just like a stack so we dont need to declare any
     // structure to keep track for us, we let the recursive calls do it for us
     been_visited[u] = VISITED; // mark u as visited
 
     // Loop overadjacency list looking for connections
-    for (int j = 0; j < (int)AdjList[u].size(); j++) {
+    for (int j = 0; j < (int)AdjList[u].size(); j++)
+    {
 
         // v is a pair representing a neighbor
         int v = AdjList[u][j];
 
         // check to see if our neighbor was visited
-        if (been_visited[v] == UNVISITED) {
+        if (been_visited[v] == UNVISITED)
+        {
             dfs(v);
         }
     }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
     int cases;
     int m, n;
     int counter;
     char largestNode;
+    int caseCount = 0;
 
     // inputs the number of cases following each item
     cin >> cases;
 
     cin.ignore();
     cin.ignore();
+    
 
-    for (int i = 0; i < cases; i++) {
+    while(caseCount < cases) {
 
         // input character/ largest node
         cin >> largestNode;
@@ -98,16 +104,26 @@ int main(int argc, char **argv) {
 
         // As long as there are unvisited nodes start a dfs on them
         counter = 0;
+        
         for (int i = 0; i < (node + 1); i++) {
+
             if (been_visited[i] == UNVISITED) {
                 dfs(i);
                 // Counts the amount of time dfs is called
                 // inorder to get the amount of subgraphs
                 counter++;
+               
             }
         }
-        cout << counter << endl
-             << endl;
+        cout << counter << endl;
+        
+       
+       caseCount++;
+
+       if(caseCount != cases) {
+         cout << endl;
+       }
+
 
         // Clear structures used
         been_visited.clear();
@@ -116,3 +132,5 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+
+
