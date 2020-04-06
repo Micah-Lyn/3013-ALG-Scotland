969f8f936b502d1c80ae660117704450acd9a9fc
Micah-Lyn <micahlyns@gmail.com>
2020-04-05 22:16:33 -0500

    .

--- Assignments/459/main.cpp
+++ Assignments/459/main.cpp
@@ -38,30 +38,26 @@ vector<vii> AdjList; // AdjList stores our edge lists
  *  int u - starting node id
  * 
  */
-void dfs(int u)
-{
+void dfs(int u) {
     // In DFS we use a stack structure to keep track of who to visit next.
     // Recursive acts just like a stack so we dont need to declare any
     // structure to keep track for us, we let the recursive calls do it for us
     been_visited[u] = VISITED; // mark u as visited
 
     // Loop overadjacency list looking for connections
-    for (int j = 0; j < (int)AdjList[u].size(); j++)
-    {
+    for (int j = 0; j < (int)AdjList[u].size(); j++) {
 
         // v is a pair representing a neighbor
         int v = AdjList[u][j];
 
         // check to see if our neighbor was visited
-        if (been_visited[v] == UNVISITED)
-        {
+        if (been_visited[v] == UNVISITED) {
             dfs(v);
         }
     }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
     int cases;
     int m, n;
@@ -74,8 +70,7 @@ int main(int argc, char **argv)
     cin.ignore();
     cin.ignore();
 
-    for (int i = 0; i < cases; i++)
-    {
+    for (int i = 0; i < cases; i++) {
 
         // input character/ largest node
         cin >> largestNode;
@@ -91,8 +86,7 @@ int main(int argc, char **argv)
         string edge;
 
         // loop to get edges to load unto vector
-        while (getline(cin, edge) && edge != "")
-        {
+        while (getline(cin, edge) && edge != "") {
 
             // subtract A from where it should start
             m = edge[0] - int('A');
@@ -104,10 +98,8 @@ int main(int argc, char **argv)
 
         // As long as there are unvisited nodes start a dfs on them
         counter = 0;
-        for (int i = 0; i < (node + 1); i++)
-        {
-            if (been_visited[i] == UNVISITED)
-            {
+        for (int i = 0; i < (node + 1); i++) {
+            if (been_visited[i] == UNVISITED) {
                 dfs(i);
                 // Counts the amount of time dfs is called
                 // inorder to get the amount of subgraphs
