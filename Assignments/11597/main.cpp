1824d48970b816532ec5581e4343d0916489048f
Micah-Lyn <micahlyns@gmail.com>
2020-04-11 18:32:08 -0500

    uva

--- Assignments/11597/main.cpp
+++ Assignments/11597/main.cpp
@@ -1,3 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Author:           Micah-Lyn Scotland
+// Email:            micahlyns@gmail.com
+// Label:            P06
+// Title:            Program 06 - 11597
+// Course:           3013
+// Semester:         Spring 2020
+//
+// Description:
+//       This programs finds the maximum number of spanning trees of a 
+//       complete undirected graph that can be formed in such a way that 
+//       no two of these spanning trees have a common edge.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
 
 using namespace std;
