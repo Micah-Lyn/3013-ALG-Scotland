cb906d68dc0d17cd2c7c923d5384a047bafd6fd2
Micah-Lyn <micahlyns@gmail.com>
2020-05-11 18:14:29 -0500

    .

--- Assignments/10261/main.cpp
+++ Assignments/10261/main.cpp
@@ -27,26 +27,29 @@ int main()
 
     while (testCase--)
     {
-
+        //ferry length
         cin >> ferry;
 
-        ferry *= 100; 
+        ferry *= 100; // converts length from metres to cm
 
         int num = 1, carL[2001], tot[2001] = {};
 
+        // while cars are being read and its more than 0
         while (cin >> carL[num] && carL[num] > 0)
         {
-
+            //add it to the total number of cars
             tot[num] = tot[num - 1] + carL[num];
             num++;
         }
 
         pair<int, int> ii = make_pair(0, 0);
 
+        //pSide turns to 1 for if cars ranging from one to i are loaded based on the length(j) to the right side of the ferry
         vector<vector<int>> pSide(num, vector<int>(ferry + 1, 0));
-
+        // stores whether the cars went on starboard size or port size
         vector<vector<string>> carSet(pSide.size(), vector<string>(pSide[0].size(), ""));
 
+        //starting position set to 1
         pSide[0][0] = 1;
 
         num--;
@@ -58,10 +61,10 @@ int main()
 
             for (int j = 0; j <= ferry; j++)
             {
-
+                // if cars from one i to i-1 were loaded sucessfully  to the right
                 if (pSide[i - 1][j] > 0)
                 {
-
+                    // load car on the port or left side
                     if (ferry - (tot[i - 1] - j) >= carL[i])
                     {
 
@@ -70,6 +73,7 @@ int main()
                         ii = make_pair(i, j);
                     }
 
+                    // load car on the starboard or right side
                     if (ferry - j >= carL[i])
                     {
 
@@ -83,10 +87,12 @@ int main()
             i++;
         }
 
+        // number of cars loaded on ferry
         cout << ii.first << endl;
 
         string answer;
 
+        // prints whether the car goes on the left side or right side
         while (carSet[ii.first][ii.second] != "")
         {
 
